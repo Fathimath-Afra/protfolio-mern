0498c010771d6d24778644973b1df70fc058ae37
Fathimath Afra <fathimathafra949@gmail.com>
2026-04-29 12:21:57 +0530

    Fix API URL path

--- client/src/api/projectService.js
+++ client/src/api/projectService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/projects';
+const API_URL = 'https://afra-portfolio-backend.onrender.com/api/projects';
 
 export const getProjects = async () => {
     try {
